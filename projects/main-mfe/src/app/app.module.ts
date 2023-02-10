d6b90e2ffca5ea97383390c28d301b8832921df8
Marcato Luca <marcato@intre.it>
2023-02-10 13:11:25 +0100

    add keycloak integration

--- projects/main-mfe/src/app/app.module.ts
+++ projects/main-mfe/src/app/app.module.ts
@@ -1,9 +1,11 @@
-import {NgModule} from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {RouterModule} from "@angular/router";
 import {APP_ROUTES} from "./app.routes";
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+import {initializeKeycloak} from "./keycloak/initializer";
 
 @NgModule({
   declarations: [
@@ -11,9 +13,17 @@ import {APP_ROUTES} from "./app.routes";
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(APP_ROUTES)
+    RouterModule.forRoot(APP_ROUTES),
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ projects/main-mfe/src/app/keycloak/initializer.ts
@@ -0,0 +1,15 @@
+import {KeycloakService} from "keycloak-angular";
+
+export function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'master',
+        clientId: 'test'
+      },
+      initOptions: {
+        onLoad: 'login-required',
+      },
+    });
+}
